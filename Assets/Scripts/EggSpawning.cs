d7f2730b1bfc54f0faea6718ba4c18c538f5408c
Bryan <bryancrim@gmail.com>
2020-01-30 15:30:24 +0100

    eggSpawning script and eggPrefab added

---
+++ Assets/Scripts/EggSpawning.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSpawning : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject eggPrefab;
+
+    private bool isSpawning = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpawning)
+        {
+            EggSpawn();
+            isSpawning = false;
+        }
+
+    }
+
+    void EggSpawn()
+    {
+        GameObject egg = Instantiate(eggPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
