bb32060cf42460eab6e1e424793a3f5c5a1ab44a
Bryan <bryancrim@gmail.com>
2020-01-31 20:14:05 +0100

    Box Preview added

--- Assets/Scripts/EggSpawning.cs
+++ Assets/Scripts/EggSpawning.cs
@@ -21,81 +21,94 @@ public class EggSpawning : MonoBehaviour
     
 
     public static bool isSpawning = true;
+    bool isRandomNmb = true;
+
+    int randomNumber = 1;
 
     //private int randomNumber;
 
     void Update()
     {
+
+        if (isRandomNmb)
+        {
+            RandomMachineNumber();
+            isRandomNmb = false;
+        }
+
         if (isSpawning)
         {
+            isRandomNmb = true;
             EggSpawn();
             isSpawning = false;
         }
+
+
         /*else
         {
             RandomMachineNumber();
         }*/
     }
 
-//    void RandomMachineNumber()
-//    {
-//        randomNumber = Random.Range(1, 5);
-//
-//        if (randomNumber == 1)
-//        {
-//            eggWhite.SetActive(true);
-//        }
-//            
-//        if (randomNumber == 2)
-//        {
-//            eggBlack.SetActive(true);
-//        }
-//            
-//        if (randomNumber == 3)
-//        {
-//            eggPurple.SetActive(true);
-//        }
-//            
-//        if (randomNumber == 4)
-//        {
-//            eggYellow.SetActive(true);
-//        }
-//    }
+   void RandomMachineNumber()
+    {
+        randomNumber = Random.Range(1, 5);
+
+        if (randomNumber == 1)
+        {
+            eggWhite.SetActive(true);
+        }
+
+        if (randomNumber == 2)
+        {
+            eggBlack.SetActive(true);
+        }
+
+        if (randomNumber == 3)
+        {
+            eggPurple.SetActive(true);
+        }
+
+        if (randomNumber == 4)
+        {
+            eggYellow.SetActive(true);
+        }
+    }
     void EggSpawn()
     {
-        int randomNumber = Random.Range(1, 5);
-        
+        //      int randomNumber = Random.Range(1, 5);
+
         if (randomNumber == 1)
         {
             GameObject egg = Instantiate(eggWhitePrefab, spawnPoint.position, spawnPoint.rotation);
-            /*eggWhite.SetActive(false);
+            eggWhite.SetActive(false);
             eggBlack.SetActive(false);
             eggPurple.SetActive(false);
-            eggYellow.SetActive(false);*/
+            eggYellow.SetActive(false);
         }
         if (randomNumber == 2)
         {
             GameObject egg = Instantiate(eggBlackPrefab, spawnPoint.position, spawnPoint.rotation);
-            /*eggWhite.SetActive(false);
+            eggWhite.SetActive(false);
               eggBlack.SetActive(false);
               eggPurple.SetActive(false);
-              eggYellow.SetActive(false);*/
+              eggYellow.SetActive(false);
         }
         if (randomNumber == 3)
         {
             GameObject egg = Instantiate(eggPurplePrefab, spawnPoint.position, spawnPoint.rotation);
-            /*eggWhite.SetActive(false);
+            eggWhite.SetActive(false);
             eggBlack.SetActive(false);
             eggPurple.SetActive(false);
-            eggYellow.SetActive(false);*/
+            eggYellow.SetActive(false);
         }
         if (randomNumber == 4)
         {
             GameObject egg = Instantiate(eggYellowPrefab, spawnPoint.position, spawnPoint.rotation);
-            /*eggWhite.SetActive(false);
+            eggWhite.SetActive(false);
                 eggBlack.SetActive(false);
                 eggPurple.SetActive(false);
-                eggYellow.SetActive(false);*/
+                eggYellow.SetActive(false);
         }
     }
 }
