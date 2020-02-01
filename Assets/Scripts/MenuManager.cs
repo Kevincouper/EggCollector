62fdafff6c677027831a665f2e9ffad347321e76
Solange <sol.sch1990@gmail.com>
2020-02-01 22:08:57 +0100

    Added script MainMenu for playing game

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panelMenuPause;
+    [SerializeField] private GameObject panelGameOver;
+    [SerializeField] private GameObject panelWin;
+    [SerializeField] private GameObject panelCredits;
+    [SerializeField] private GameObject panelMenuStartGame;
+
+    public void LoadScene (string sceneName)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void DesactivateMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ActivateMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ActivatePanelCredits()
+    {
+        panelCredits.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void DesactivatePanelCredits()
+    {
+        panelCredits.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ActivatePanelMenuStartGame()
+    {
+        panelMenuStartGame.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void DesactivatePanelMenuStartGame()
+    {
+        panelMenuStartGame.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
