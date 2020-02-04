96310282b848499e40a9ffacf4d3b57b1558b54f
Solange <sol.sch1990@gmail.com>
2020-02-04 12:06:04 +0100

    Added testPrefabsSolange folder with test egg spawner randomin scene TestSolange

---
+++ Assets/Scripts/TestsScriptsSolange/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] eggObjects;
+
+    void Start()
+    {
+        SpawnRandom();
+    }
+
+    public void SpawnRandom()
+    {
+        int index = Random.Range(0, eggObjects.Length);
+        Instantiate(eggObjects[index], transform.position, Quaternion.identity);
+    }
+}
