55a46e00c6867cc9dec0939d086e2dd12706fe9d
Solange <sol.sch1990@gmail.com>
2020-02-04 15:44:29 +0100

    Added script MatrixGrid

---
+++ Assets/Scripts/TestsScriptsSolange/MatrixGrid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixGrid 
+{
+    [SerializeField] private static int row = 10;
+    [SerializeField] private static int column = 20;
+
+    public Transform[,] grid = new Transform[row, column];
+
+    public static Vector2 RoundVector(Vector2 vect)
+    {
+        return new Vector2(Mathf.Round(vect.x), Mathf.Round(vect.y));
+    }
+}
