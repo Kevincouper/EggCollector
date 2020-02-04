b9cd853b9c25650457d7d6491da3f604766901be
Solange <sol.sch1990@gmail.com>
2020-02-04 11:33:36 +0100

    Added GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+        Time.timeScale = 1;
+        Score.score = 0;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 0;
+    }
+}
