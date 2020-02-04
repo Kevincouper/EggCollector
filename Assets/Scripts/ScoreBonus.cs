78388e7f0d9925bc358b082e060060893e242e5a
Bryan <bryancrim@gmail.com>
2020-02-04 14:26:54 +0100

    ScoreBonus Scripts added

---
+++ Assets/Scripts/ScoreBonus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBonus : MonoBehaviour
+{
+    [SerializeField] private string eggName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == eggName)
+        {
+            Score.score += 5;
+        }
+    }
+}
